73dc37a068418e5b3eca6498db5d763a3e8a223e
maihannn <maicapalillo15@gmail.com>
2024-01-22 12:05:07 +0800

    it worked, done

--- product_and_sum_of_2_numbers.py
+++ product_and_sum_of_2_numbers.py
@@ -5,3 +5,28 @@
 
 # If the answer is equal or less than 1000, get the product.
 # If not, get their sum.
+
+
+# get the two integers
+
+number1 = int(input("Enter the First Number: "))
+number2 = int(input("Enter the Second Number: "))
+
+
+def get_answer (number1, number2):
+    
+    product = number1*number2
+    
+    if (product <= 1000):
+        return product
+        
+    else:
+        return number1 +number2
+
+
+answer = get_answer (number1, number2)
+print ("\nThe answer is: ", answer)
+        
+
+
+

f91a25fcaf5f8fd60390c4d9e26f861e9f6cc85f
maihannn <maicapalillo15@gmail.com>
2024-01-22 11:17:42 +0800

    added pseudocode

---
+++ product_and_sum_of_2_numbers.py
@@ -0,0 +1,7 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 1: Calculate the multiplication and sum of two numbers.
+
+# If the answer is equal or less than 1000, get the product.
+# If not, get their sum.
